c5c71e8b1032b005d5a303a8fa4b36a2641a9e84
Zeta Labs <zetalabs@gmail.com>
2015-11-17 11:21:53 +0800

    update frameworks/av/media/CedarX-Projects/CedarXAndroid/

--- frameworks/av/media/CedarX-Projects/CedarXAndroid/Android.mk
+++ frameworks/av/media/CedarX-Projects/CedarXAndroid/Android.mk
@@ -6,7 +6,7 @@ include $(LOCAL_PATH)/../Config.mk
 ifeq ($(CEDARX_ANDROID_VERSION),4)
 include $(LOCAL_PATH)/Gingerbread/Android.mk
 else
-include $(LOCAL_PATH)/IceCreamSandwich/Android.mk
+include $(LOCAL_PATH)/KitKat/Android.mk
 endif
 
 $(info CEDARX_PRODUCTOR: $(CEDARX_PRODUCTOR))
